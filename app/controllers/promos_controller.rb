7fc208bd3ff12d18b99ff77f4d55942a7c12c888
Hans Dushanthakumar <hansrd_98@yahoo.com>
2016-08-13 16:08:23 +1000

    ww-1816 Added api to return Promos

--- app/controllers/promos_controller.rb
+++ app/controllers/promos_controller.rb
@@ -1,6 +1,11 @@
 class PromosController < ApplicationController
   def index
-    @promos = Promo.all
+    if params[:environment] != nil
+      env=Environment.find_by(name: params[:environment])
+      @promos = Array(Promo.find_by(environment: env))
+    else
+      @promos = Promo.all
+    end
     respond_to do |format|
       format.html
       format.json { render(json: @promos) }
