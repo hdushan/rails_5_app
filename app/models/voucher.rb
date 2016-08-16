1136005bba5b8c05df3b15ce19ce5fceb50bdeac
Hans Dushanthakumar <hansrd_98@yahoo.com>
2016-08-16 22:49:18 +1000

    ww-1816 Fixed rubocop suggestions

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -3,11 +3,9 @@ class Voucher < ApplicationRecord
   self.per_page = 10
 
   def self.unexpired(environment: 'battlefield', count: 1)
-    count = 1 if count.nil? or  count == 0
-    Voucher.where(environment_id: Environment.where(name: environment).first, in_use: false).limit(count)
+    count = 1 if count.nil? || count.zero?
     expiry_dates = Voucher.arel_table[:expiry_date]
-
     Voucher.where(environment_id: Environment.where(name: environment).first)
-                         .where(expiry_dates.gt(Date.today)).limit(count)
+           .where(expiry_dates.gt(Date.today)).limit(count)
   end
 end
