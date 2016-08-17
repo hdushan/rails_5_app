5bdef8dcfde28a8f50fd23f37b1d3400bac049cf
Hans Dushanthakumar <hansrd_98@yahoo.com>
2016-08-17 22:51:44 +1000

    ww-1816 UI tweaks for tables

--- spec/controllers/credit_cards_controller_spec.rb
+++ spec/controllers/credit_cards_controller_spec.rb
@@ -2,9 +2,21 @@ require 'rails_helper'
 
 RSpec.describe CreditCardsController, type: :controller do
   describe 'GET #index' do
-    it 'returns http success' do
+    # let(:environment) { build :environment, name: 'MyEnv', url: 'http://myenv', is_prod_data: false }
+    # let(:credit_card) { build :credit_card, card_number: '12345', cvv: '999', environment: environment }
+
+    before do
+      # allow(Environment).to receive(:all) { Environment.where(id: [environment].map(&:id)) }
+      # allow(CreditCard).to receive(:all) { CreditCard.where(id: [credit_card].map(&:id)) }
       get :index
+    end
+
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
+    end
+
+    xit 'sets up environments correctly' do
+      expect(assigns(:credit_cards)).to eq([credit_card])
     end
   end
 
